ef54491629fc6d065c06cd9649452982609d62f2
DevWithShakil <m.shakilkhan720@gmail.com>
2025-07-15 02:05:39 +0600

    font awesome icon added

--- index.php
+++ index.php
@@ -6,10 +6,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Age Calculator</title>
     <link rel="stylesheet" href="style.css">
+
+    <!-- font awesome -->
+    <script src="https://kit.fontawesome.com/defbdbe434.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
-
+    <div class="container">
+        <h1>Let's Find Out Your Age!</h1>
+        <div id="dob">
+            <h3>Date of Birth</h3>
+            <input type="date">
+        </div>
+        <br>
+        <div id="current-date">
+            <h3>Current Date</h3>
+            <input type="date">
+        </div>
+    </div>
 </body>
 
 </html>
